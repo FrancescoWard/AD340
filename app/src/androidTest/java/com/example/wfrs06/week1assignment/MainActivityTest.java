f4281ca92b8d2c6a5c3aff2487d0e67f67a290f8
FrancescoWard <wfrs06@gmail.com>
2018-04-11 11:45:23 -0700

    Initial commit

---
+++ app/src/androidTest/java/com/example/wfrs06/week1assignment/MainActivityTest.java
@@ -0,0 +1,51 @@
+package com.example.wfrs06.week1assignment;
+
+import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
+import android.view.View;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.*;
+
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule
+            = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void run() {
+        onView(withId(R.id.hello_world_text))
+                .check(matches(withText(R.string.output)));
+
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
+
+        onView(withText(R.string.pic))
+                .perform(click());
+
+        onView(withId(R.id.space_img))
+                .check(matches(isDisplayed()));
+
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
+
+        onView(withText(R.string.restore))
+                .perform(click());
+
+        onView(withId(R.id.hello_world_text))
+                .check(matches(withText(R.string.output)));
+
+    }
+}
